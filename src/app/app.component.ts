c5661a847cd9cb3f10d005774c32206dad5fcee5
nguyenminh92 <51382520+nguyenminh92@users.noreply.github.com>
2022-04-14 20:37:56 -0400

    Implement azure b2c authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
+import { InteractionStatus, RedirectRequest } from '@azure/msal-browser';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spa-identity-poc';
+  loginDisplay = false;
+  private readonly _destroying$ = new Subject<void>();
+
+  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration, private broadcastService: MsalBroadcastService, private authService: MsalService) { }
+
+  ngOnInit() {
+
+    this.broadcastService.inProgress$
+    .pipe(
+      filter((status: InteractionStatus) => status === InteractionStatus.None),
+      takeUntil(this._destroying$)
+    )
+    .subscribe(() => {
+      this.setLoginDisplay();
+    })
+  }
+
+  login() {
+    let authRequestConfig: RedirectRequest;
+
+    if (this.msalGuardConfig.authRequest){
+      authRequestConfig = { ...this.msalGuardConfig.authRequest } as RedirectRequest;
+      
+      authRequestConfig.loginHint = "bob@contoso.com";
+
+      this.authService.loginRedirect(authRequestConfig);
+      // this.authService.loginRedirect({...this.msalGuardConfig.authRequest} as RedirectRequest);
+    } 
+    // else {
+    //   this.authService.loginRedirect();
+    // }
+  }
+
+  logout() { 
+    this.authService.logoutRedirect({
+      postLogoutRedirectUri: 'http://localhost:4200'
+    });
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
 }
---
+++ src/app/auth-config.ts
@@ -0,0 +1,52 @@
+import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+export const b2cPolicies = {
+    names: {
+        signUpSignIn: "B2C_1_signupsignin1",
+        //  editProfile: "b2c_1_edit_profile_v2"
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: "https://expeedsoft.b2clogin.com/expeedsoft.onmicrosoft.com/B2C_1_signupsignin1/oauth2/v2.0/authorize?domain_hint=expeed.com",
+        },
+        //  editProfile: {
+        //      authority: "https://your-tenant-name.b2clogin.com/your-tenant-name.onmicrosoft.com/b2c_1_edit_profile_v2"
+        //  }
+    },
+    authorityDomain: "expeedsoft.b2clogin.com"
+};
+
+
+export const msalConfig: Configuration = {
+    auth: {
+        clientId: 'ecf21b44-5b0d-42ce-802d-7654d9b757eb',
+        authority: b2cPolicies.authorities.signUpSignIn.authority,
+        knownAuthorities: [b2cPolicies.authorityDomain],
+        redirectUri: '/'
+    },
+    cache: {
+        cacheLocation: BrowserCacheLocation.LocalStorage,
+        storeAuthStateInCookie: isIE,
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (logLevel, message, containsPii) => {
+                console.log(message);
+            },
+            logLevel: LogLevel.Verbose,
+            piiLoggingEnabled: false
+        }
+    }
+}
+
+export const protectedResources = {
+    todoListApi: {
+        endpoint: "http://localhost:5000/api/todolist",
+        scopes: ["https://expeedsoft.onmicrosoft.com/api/tasks.read"],
+    },
+}
+export const loginRequest = {
+    scopes: []
+};
